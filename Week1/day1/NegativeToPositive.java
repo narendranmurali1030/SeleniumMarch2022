82f2ceb29a74f449f467626cc350689910df0692
narendranmurali1030 <62905177+narendranmurali1030@users.noreply.github.com>
2022-03-21 16:02:23 +0530

    Add files via upload

---
+++ Week1/day1/NegativeToPositive.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class NegativeToPositive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int number = -20;
+
+		if (number >= 0) {
+			System.out.println("Given Number is " + number + " . Hence it a positive number by default");
+		} else {
+			System.out.println(-number);
+		}
+
+	}
+
+}
