0a135cc97ab556ef8c071419a362bdb7e8a15983
narendranmurali1030 <62905177+narendranmurali1030@users.noreply.github.com>
2022-03-21 16:02:56 +0530

    Add files via upload

---
+++ Week1/day2/MyCalculator.java
@@ -0,0 +1,15 @@
+package week1.day2;
+
+public class MyCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Calculator name = new Calculator();
+		System.out.println(name.add(10, 20, 30));
+		System.out.println(name.subtract(10, 20));
+		System.out.println(name.multiply(20.45f, 100.589f));
+		System.out.println(name.divide(45.6896, 25.5952));
+	}
+
+}
